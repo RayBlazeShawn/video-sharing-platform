23edad2a688c7ff026d55151a92d2bc4c970e806
rayblazeshawn <rishabh.yupp@gmail.com>
2024-06-26 20:27:56 +0530

    feat: add more user functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -19,15 +25,22 @@ router.route("/register").post(
   registerUser);
 
 
-
-
 router.route("/login").post(
-  loginUser
-)
+  loginUser,
+);
+
 //secured routes
 
-router.route("/logout").post(verifyJwt,logoutUser)
+router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;
